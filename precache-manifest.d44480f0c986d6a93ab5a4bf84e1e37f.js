a837133eb6b4125ea5d58eac20e508fe8d82dc7c
Andrey Zinoviev <dronis_dronis@mail.ru>
2021-02-05 19:21:36 +0300

    Updates

---
+++ precache-manifest.d44480f0c986d6a93ab5a4bf84e1e37f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52620db828bdc1066959059ccb8176d1",
+    "url": "/react-mesto-auth/index.html"
+  },
+  {
+    "revision": "34d1dcfbec939373284b",
+    "url": "/react-mesto-auth/static/css/main.01d732fa.chunk.css"
+  },
+  {
+    "revision": "0db5081f4b2aabab6af4",
+    "url": "/react-mesto-auth/static/js/2.87714648.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-mesto-auth/static/js/2.87714648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d1dcfbec939373284b",
+    "url": "/react-mesto-auth/static/js/main.efe1e025.chunk.js"
+  },
+  {
+    "revision": "26b811961fdce21added",
+    "url": "/react-mesto-auth/static/js/runtime-main.0e88f56d.js"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/react-mesto-auth/static/media/Vector.855a8c98.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/react-mesto-auth/static/media/like.6c74d9b5.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/react-mesto-auth/static/media/like_active.9fdebb6b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34ba8279b190c32ceff0bc4fcf8503eb.js"
+  "/react-mesto-auth/precache-manifest.d44480f0c986d6a93ab5a4bf84e1e37f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-mesto-auth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
