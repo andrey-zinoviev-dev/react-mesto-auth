f8b3887fdc18eb5009ac2494d9a66b2471f26442
Andrey Zinoviev <dronis_dronis@mail.ru>
2021-02-05 19:13:06 +0300

    Updates

---
+++ precache-manifest.34ba8279b190c32ceff0bc4fcf8503eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf0f09bccb2ced42ddcff7c69bb7a42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91909954a8418a9462b2",
+    "url": "/static/css/main.7b4f0492.chunk.css"
+  },
+  {
+    "revision": "0db5081f4b2aabab6af4",
+    "url": "/static/js/2.87714648.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.87714648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91909954a8418a9462b2",
+    "url": "/static/js/main.faf2fcf3.chunk.js"
+  },
+  {
+    "revision": "e0a3161bb91b78851583",
+    "url": "/static/js/runtime-main.1de49aa2.js"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/static/media/Vector.855a8c98.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/static/media/like.6c74d9b5.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/static/media/like_active.9fdebb6b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2095982b709bcb05f9a1d37bde67c854.js"
+  "/precache-manifest.34ba8279b190c32ceff0bc4fcf8503eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
