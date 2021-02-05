e7af6f4037f6289be224a55ea7d969420d6719f7
Andrey Zinoviev <dronis_dronis@mail.ru>
2021-02-05 18:31:11 +0300

    Updates

---
+++ precache-manifest.ef606193bca72e89d49e26c7aca4f2b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31a18db2708462f7ff9b942d8bbab7a",
+    "url": "./react-mesto-auth/index.html"
+  },
+  {
+    "revision": "a0a4a453b941c7097230",
+    "url": "./react-mesto-auth/static/css/main.bef3e652.chunk.css"
+  },
+  {
+    "revision": "0db5081f4b2aabab6af4",
+    "url": "./react-mesto-auth/static/js/2.87714648.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./react-mesto-auth/static/js/2.87714648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a4a453b941c7097230",
+    "url": "./react-mesto-auth/static/js/main.ff300cfd.chunk.js"
+  },
+  {
+    "revision": "32d08136d938b7c790b0",
+    "url": "./react-mesto-auth/static/js/runtime-main.5871a253.js"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "./react-mesto-auth/static/media/Vector.855a8c98.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "./react-mesto-auth/static/media/like.6c74d9b5.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "./react-mesto-auth/static/media/like_active.9fdebb6b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d3c38e78bdcb90e5de63fd4a5d48da77.js"
+  "./react-mesto-auth/precache-manifest.ef606193bca72e89d49e26c7aca4f2b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./react-mesto-auth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
